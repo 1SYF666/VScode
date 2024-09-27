6d1022c9e9f4ecd60e3f819874a70cac46afe89e
1SYF666 <99598845+1SYF666@users.noreply.github.com>
2024-09-27 10:57:29 +0800

    second

--- CPP/test_0920/function.cpp
+++ CPP/test_0920/function.cpp
@@ -1,6 +1,6 @@
 #include"function.h"
 
-void comfunctions::getErrRate(char *decode_output, int decode_len, char *basedata, int basedatalen, float *iErrRate)
+void Comfunctions::getErrRate(char *decode_output, int decode_len, char *basedata, int basedatalen, float *iErrRate)
 {
     int iErrNum = basedatalen * 0.1; // 误码容限
     float rate_each = 0;
@@ -47,103 +47,102 @@ void comfunctions::getErrRate(char *decode_output, int decode_len, char *basedat
         }
     }
 
-    *iErrRate = rate_each/compare_time;
+    *iErrRate = rate_each / compare_time;
 }
 
 // 按字节流读取float型文件
-int comfunctions::ReadFile(const string& path ,float *outputdata,int& numFloats)
+int Comfunctions::ReadFile(const string &path, float *outputdata, int &numFloats)
 {
     // 打开文件，使用二进制模式读取
-    ifstream file(path,ios::binary);
+    ifstream file(path, ios::binary);
 
     // 检查文件是否成功打开
-    if(!file.is_open())
+    if (!file.is_open())
     {
-        cerr<<"无法打开文件！"<<endl;
+        cerr << "无法打开文件！" << endl;
         return -1;
     }
 
     // 移动文件指针到文件末尾以计算文件的大小
-    file.seekg(0,ios::end);
-    size_t fileSize = file.tellg();    // 获取文件的大小
-    file.seekg(0,ios::beg);            // 将文件指针移回开头
+    file.seekg(0, ios::end);
+    size_t fileSize = file.tellg(); // 获取文件的大小
+    file.seekg(0, ios::beg);        // 将文件指针移回开头
 
     // 分配缓冲区来存储文件内容
     char *buffer = new char[fileSize];
 
     // 读取文件内容到缓冲区中
-    file.read(buffer,fileSize);
-    if(fileSize%sizeof(float)!=0)
+    file.read(buffer, fileSize);
+    if (fileSize % sizeof(float) != 0)
     {
-        cerr<<"文件大小不是float 大小的整数倍！"<<endl;
+        cerr << "文件大小不是float 大小的整数倍！" << endl;
     }
 
-    numFloats = fileSize / sizeof(float);       // 计算有多少个 float 数据
+    numFloats = fileSize / sizeof(float); // 计算有多少个 float 数据
 
     // 关闭文件
     file.close();
 
-    memcpy(outputdata,buffer,fileSize);
+    memcpy(outputdata, buffer, fileSize);
 
     return 0;
 }
 
-int comfunctions::ReadFile(const string& path ,complex *outputdata,int& floatnum,int& complexnum)
+int Comfunctions::ReadFile(const string &path, complex *outputdata, int &floatnum, int &complexnum)
 {
-        // 打开文件，使用二进制模式读取
-    ifstream file(path,ios::binary);
+    // 打开文件，使用二进制模式读取
+    ifstream file(path, ios::binary);
 
     // 检查文件是否成功打开
-    if(!file.is_open())
+    if (!file.is_open())
     {
-        cerr<<"无法打开文件！"<<endl;
+        cerr << "无法打开文件！" << endl;
         return -1;
     }
 
     // 移动文件指针到文件末尾以计算文件的大小
-    file.seekg(0,ios::end);
-    size_t fileSize = file.tellg();    // 获取文件的大小
-    file.seekg(0,ios::beg);            // 将文件指针移回开头
+    file.seekg(0, ios::end);
+    size_t fileSize = file.tellg(); // 获取文件的大小
+    file.seekg(0, ios::beg);        // 将文件指针移回开头
 
     // 分配缓冲区来存储文件内容
     char *buffer = new char[fileSize];
 
     // 读取文件内容到缓冲区中
-    file.read(buffer,fileSize);
-    if(fileSize%sizeof(float)!=0)
+    file.read(buffer, fileSize);
+    if (fileSize % sizeof(float) != 0)
     {
-        cerr<<"文件大小不是float 大小的整数倍！"<<endl;
+        cerr << "文件大小不是float 大小的整数倍！" << endl;
     }
 
-    floatnum = fileSize / sizeof(float) ;       // 计算有多少个 float 数据
-    complexnum = floatnum / 2; 
-    
+    floatnum = fileSize / sizeof(float); // 计算有多少个 float 数据
+    complexnum = floatnum / 2;
+
     // 关闭文件
     file.close();
 
-    memcpy(outputdata,buffer,fileSize);
+    memcpy(outputdata, buffer, fileSize);
 
     return 0;
 }
-
 
 // 按float型数组按字节流写入文件
 // path:写入文件路径
 // data:需要写入的float型数组
 // sizenum:需要写入的float型数组长度
-int comfunctions::WriteFile(const string& path ,float* data,int sizenum)
+int Comfunctions::WriteFile(const string &path, float *data, int sizenum)
 {
     // 以二进制模式打开文件
-    ofstream file(path,ios::binary);
+    ofstream file(path, ios::binary);
 
-    if(!file.is_open())
+    if (!file.is_open())
     {
-        cerr<<"文件无法打开！"<<endl;
+        cerr << "文件无法打开！" << endl;
         return -1;
     }
 
     // 将float 数组转换为字节流并写入文件
-    file.write(reinterpret_cast<const char*>(data),sizeof(float)*sizenum);
+    file.write(reinterpret_cast<const char *>(data), sizeof(float) * sizenum);
 
     // 关闭文件
     file.close();
@@ -151,46 +150,138 @@ int comfunctions::WriteFile(const string& path ,float* data,int sizenum)
     return 0;
 }
 
-  int comfunctions::WriteFile(const string& path ,complex * data,int complexnum)
-  {
-        // 以二进制模式打开文件
-    ofstream file(path,ios::binary);
-
-    if(!file.is_open())
-    {
-        cerr<<"文件无法打开！"<<endl;
-        return -1;
-    }
-
-    // 将float 数组转换为字节流并写入文件
-    file.write(reinterpret_cast<const char*>(data),sizeof(float)*complexnum*2);
-
-    // 关闭文件
-    file.close();
-
-    return 0;
-  }  
-
-int main()
+int Comfunctions::WriteFile(const string &path, complex *data, int complexnum)
 {
-    comfunctions comfunction;
-    int floatnum = 0;
-    int cpmlexnum = 0 ;
-    // float* output = new float[655360];
-    complex* output = new complex[655360];
-    string filePath = "C:\\Users\\PC\\Desktop\\outdata.dat";  
+    // 以二进制模式打开文件
+    ofstream file(path, ios::binary);
 
-    comfunction.ReadFile(filePath,output,floatnum,cpmlexnum);
-    
-    for (size_t i = 0; i < cpmlexnum; i++)
+    if (!file.is_open())
     {
-        cout<<output[i].I<<"  "<<output[i].Q<<endl;                              
+        cerr << "文件无法打开！" << endl;
+        return -1;
     }
 
-    filePath = "C:\\Users\\PC\\Desktop\\outdata.dat";  
-    comfunction.WriteFile(filePath,output,cpmlexnum);
+    // 将float 数组转换为字节流并写入文件
+    file.write(reinterpret_cast<const char *>(data), sizeof(float) * complexnum * 2);
 
+    // 关闭文件
+    file.close();
 
     return 0;
 }
 
+/*
+    input: num整数(十进制)，类型uint8_t
+    return value: num-->格雷码-->格雷码对应整数(十进制)
+*/
+uint8_t Comfunctions::decimalToGray(uint8_t num)
+{
+    return num ^ (num >> 1); // num >> 1右移后左边补零，^为按位异或操作
+}
+
+/*
+    input:
+        parameter1: value整数(十进制)，类型uint8_t,
+        parameter2: bitnum为value转化为二进制的最长位数
+    return value:  value转化为二进制，类型 string
+*/
+string Comfunctions::extractLastSixBitsToString(uint8_t value, int bitnum)
+{
+    string binaryString;
+    if (bitnum == 2)
+    {
+        value = value & 0b11;
+        binaryString = bitset<2>(value).to_string();
+    }
+    else if (bitnum == 3)
+    {
+        value = value & 0b111;
+        binaryString = bitset<3>(value).to_string();
+    }
+    else if (bitnum == 4)
+    {
+        value = value & 0b1111;
+        binaryString = bitset<4>(value).to_string(); // bitset 是 C++ 标准库中的一个模板类，用于表示固定大小的位集合。
+    }
+    else if (bitnum == 5)
+    {
+        value = value & 0b11111;
+        binaryString = bitset<5>(value).to_string();
+    }
+    else if (bitnum == 6)
+    {
+        value = value & 0b111111; // 按位与，保证低6位的值
+        // 将提取后的值转换为6位的二进制字符串，这里的 <6> 指定了位集合的大小为6位。
+        binaryString = bitset<6>(value).to_string(); // to_string() 成员函数将 bitset 对象转换为一个字符串
+    }
+    else if (bitnum == 7)
+    {
+        value = value & 0b1111111; // 按位与，保证低7位的值
+        binaryString = bitset<7>(value).to_string();
+    }
+    else if (bitnum == 8)
+    {
+        value = value & 0b11111111;
+        binaryString = bitset<8>(value).to_string();
+    }
+    return binaryString;
+}
+
+/*
+    function: 将char类型元素写出bin文件
+    input:
+        parameter1: input数组(十进制)，类型uint8_t*,
+        parameter2: inum为input数组长度
+        parameter3: bitnum为value转化为二进制的最长位数
+    return value:  value转化为二进制，类型 string
+*/
+
+void Comfunctions::write_bin(const string &filepath, uint8_t *input, int inum, int bitnum)
+{
+    for (int i = 0; i < inum; i++)
+    {
+        input[i] = decimalToGray(input[i]);
+    }
+
+    // 存储连续的字符串
+    string concatenatedString; // string 可以看作是一个动态数组，能够根据需要自动调整大小
+    if ((inum * bitnum) % 8 == 0)
+    {
+        // 提取每个元素的后6位，并拼接成一个长字符串
+        for (int i = 0; i < inum; i++)
+        {
+            string binaryString = extractLastSixBitsToString(input[i], bitnum);
+            concatenatedString += binaryString; // 将 extractLastSixBitsToString 函数返回的字符串
+                                                // binaryString 连接到 concatenatedString 变量上。
+        }
+    }
+    else
+    {
+        for (int i = 0; i < inum; i++)
+        {
+            string binaryString = extractLastSixBitsToString(input[i], bitnum);
+            concatenatedString += binaryString;
+        }
+        // 添加bitnum个二进制的0
+        for (int i = 0; i < 8 - ((inum * bitnum) % 8); i++)
+        {
+            concatenatedString += '0'; // 在每次迭代中，将一个 '0' 字符追加到 concatenatedString 的末尾。
+        }
+    }
+
+    int charnum = ceil(inum * bitnum / 8.0); // 需要的char型字符个数
+    // vector<char>chars(charnum);
+    char *chars = new char[charnum];
+    // 将字符串拆分并存储到chars数组中
+    for (int i = 0; i < charnum; i++)
+    {
+        string subStr = concatenatedString.substr(i * 8, 8); // 从i*8开始提取8位
+        char value = static_cast<char>(stoi(subStr, nullptr, 2));
+        chars[i] = value;
+    }
+
+    // 写入二进制文件
+    ofstream outfile(filepath, ios::out | ios::binary);
+    outfile.write(chars, charnum);
+    outfile.close();
+}
--- CPP/test_0920/function.h
+++ CPP/test_0920/function.h
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
+#include <math.h>
+#include <bitset>
 using namespace std;
 
 struct complex
@@ -9,28 +12,36 @@ struct complex
     float Q;
 };
 
-class comfunctions
-{private:
+class Comfunctions
+{
+private:
     /* data */
 
 public:
-    comfunctions(/* args */);
-    ~comfunctions();
+    // Comfunctions(/* args */);
+    // ~Comfunctions();
 
     void getErrRate(char *decode_output, int decode_len, char *basedata, int basedatalen, float *iErrRate);
-    int ReadFile(const string& path ,float *outputdata,int& numFloats);
-    int WriteFile(const string& path ,float* data,int sizenum);
-    int ReadFile(const string& path ,complex *outputdata,int& numFloats,int& complexnum);
-    int WriteFile(const string& path ,complex * data,int complexnum);
-    
+
+    int ReadFile(const string &path, float *outputdata, int &numFloats);
+
+    int WriteFile(const string &path, float *data, int sizenum);
+
+    int ReadFile(const string &path, complex *outputdata, int &numFloats, int &complexnum);
+
+    int WriteFile(const string &path, complex *data, int complexnum);
+
+    string extractLastSixBitsToString(uint8_t value, int bitnum);
+
+    uint8_t decimalToGray(uint8_t num);
+
+    void write_bin(const string &filepath, uint8_t *input, int inum, int bitnum);
 };
 
-comfunctions::comfunctions(/* args */)
-{
-}
+// Comfunctions::Comfunctions(/* args */)
+// {
+// }
 
-comfunctions::~comfunctions()
-{
-}
-
-
+// Comfunctions::~Comfunctions()
+// {
+// }

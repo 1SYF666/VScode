5802ec095b11a90cc32470bca7385a79ba50e379
1SYF666 <99598845+1SYF666@users.noreply.github.com>
2025-03-27 20:23:43 +0800

    add some code

---
+++ Leetcode/129.求根节点到叶节点数字之和.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=129 lang=cpp
+ *
+ * [129] 求根节点到叶节点数字之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        return sumNumbersDFS(root, 0);
+    }
+    int sumNumbersDFS(TreeNode* root, int sum) {
+        if (!root) return 0;
+        sum = sum * 10 + root->val;
+        if (!root->left && !root->right) return sum;
+        return sumNumbersDFS(root->left, sum) + sumNumbersDFS(root->right, sum);
+    }
+
+};
+// @lc code=end
+

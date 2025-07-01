43c4601e942a12cb6cdafda9418c014f968b4f74
1SYF666 <2832195814@qq.com>
2025-07-01 14:36:05 +0800

    first: modify new method on dynamic programming to solve the problem.

--- TEST/Knapsack_Problem.cpp
+++ TEST/Knapsack_Problem.cpp
@@ -18,23 +18,23 @@ example 1：
 
 // 回溯法（暴力解决）
 
-void backtracking( int index, int currentWeight, int currentValue, int maxweight,
-                 const vector<int>& weight,const vector<int>& value,int& maxValue)
+void backtracking(int index, int currentWeight, int currentValue, int maxweight,
+                  const vector<int> &weight, const vector<int> &value, int &maxValue)
 {
     // 退出
-    if (index >= weight.size()||currentWeight>maxweight)
+    if (index >= weight.size() || currentWeight > maxweight)
     {
-        maxValue = max(maxValue,currentValue);
+        maxValue = max(maxValue, currentValue);
         return;
     }
 
     // 不选择当前数据
-    backtracking(index+1,currentWeight,currentValue,maxweight,weight,value,maxValue);
+    backtracking(index + 1, currentWeight, currentValue, maxweight, weight, value, maxValue);
     // 选择当前数据
-    if(currentWeight+weight[index]<=maxweight)
+    if (currentWeight + weight[index] <= maxweight)
     {
-        backtracking(index+1,currentWeight+weight[index],currentValue+value[index],
-                    maxweight,weight,value,maxValue);
+        backtracking(index + 1, currentWeight + weight[index], currentValue + value[index],
+                     maxweight, weight, value, maxValue);
     }
 }
 
@@ -45,21 +45,57 @@ void example1(int &finalvalue)
     vector<int> weight = {1, 3, 4};
     vector<int> value = {15, 20, 30};
     int maxValue = 0;
-    backtracking(0,0,0,maxweight,weight,value,maxValue);
+    backtracking(0, 0, 0, maxweight, weight, value, maxValue);
     finalvalue = maxValue;
 }
 
 // 动态规划
+int DynamicProgramming_exp1()
+{
+    // 物品:下标索引0 1 2
+    int maxweight = 4;
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    // dp数组
+    vector<vector<int>> dp(weight.size(), vector<int>(maxweight + 1, 0));
 
+    // 初始化第一行
+    for (int j = weight[0]; j <= maxweight; j++)
+    {
+        dp[0][j] = value[0];
+    }
 
+    // 开始遍历
+    for (int i = 1; i < weight.size(); i++)
+    {
+        for (int j = 0; j <= maxweight; j++)
+        {
+            if (j < weight[i])
+            {
+                dp[i][j] = dp[i-1][j];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+            }
+        }
+    }
 
+    return dp[weight.size() - 1][maxweight];
+}
+
+int TestExample1()
+{
+    // int finalvalue = 0;
+    // example1(finalvalue);
+    int finalvalue = DynamicProgramming_exp1();
+    cout << "最大价值是：" << finalvalue << endl;
+    return 0;
+}
 
 
 int main()
 {
-    int finalvalue = 0;
-    example1(finalvalue);
-    cout<<"最大价值是："<<finalvalue<<endl;
+    
     return 0;
 }
-

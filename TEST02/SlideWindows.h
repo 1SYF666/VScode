65e2bb5d795c4791fc2674578bd4bd5f2edcab9e
1SYF666 <2832195814@qq.com>
2025-08-07 19:48:45 +0800

    add some new functions

--- TEST02/SlideWindows.h
+++ TEST02/SlideWindows.h
@@ -1,8 +1,90 @@
 #include <unordered_map>
 #include <string>
 #include <vector>
+#include <deque>
 #include <unordered_set>
 using namespace std;
+
+vector<int> maxslidewindows(vector<int> &nums, int k)
+{
+    vector<int> result;
+    // front 大 --- back 小
+    deque<int> q;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        // 出--因为窗口长度就是k 
+        while (q.size()&&q.front()==i-k)
+        {
+            q.pop_front();
+        }
+        
+        // 从队尾入
+        while (q.size() && nums[i] > nums[q.back()])
+        {
+            q.pop_back();
+        }
+        q.push_back(i);
+        // 弹出
+        if (i - k + 1 >= 0)
+        {
+            result.push_back(nums[q.front()]);
+        }
+    }
+    return result;
+}
+
+/*
+时间:20250807 18:29
+76. 最小覆盖子串
+困难
+相关标签
+提示
+给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+*/
+
+string minWindow(string s, string t)
+{
+    int n = s.size();
+
+    unordered_map<char, int> mp;
+    for (int i = 0; i < t.size(); i++)
+    {
+        mp[t[i]]++;
+    }
+    // 窗口状态：覆盖t中所有字符
+    // [left right] 满足这个状态，[left,right+1]一定满足这个状态  left++
+    // [left right] 不满足这个状态，[left+1,right]一定不满足这个状态
+
+    int totalCnt = 0; // 窗口中有效字符数量
+    int ans_left = 0, ans_right = s.size();
+
+    for (int left = 0, right = 0; right < n; right++)
+    {
+        mp[s[right]]--;
+        if (mp[s[right]] >= 0)
+        {
+            totalCnt++; // 窗口中字符有效
+        }
+        while (totalCnt == t.size())
+        {
+            // 满足状态
+            if ((ans_right - ans_left) > (right - left))
+            {
+                ans_left = left;
+                ans_right = right;
+            }
+
+            mp[s[left]]++;
+            if (mp[s[left]] > 0)
+            {
+                totalCnt--;
+            }
+            left++;
+        }
+        // 不满足状态
+    }
+    return ans_right == s.size() ? "" : s.substr(ans_left, ans_right - ans_left + 1);
+}
 
 /*
 时间:20250807 16:01
@@ -132,4 +214,5 @@ private:
     DListNode *tail;
     int size;
     int capacity;
+    
 };

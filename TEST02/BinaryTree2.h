b6fee3507861dd5db4490e1e9e6fa43a3f503765
1SYF666 <2832195814@qq.com>
2025-08-20 20:54:21 +0800

    add some new functions.

--- TEST02/BinaryTree2.h
+++ TEST02/BinaryTree2.h
@@ -1,4 +1,6 @@
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 struct TreeNode
@@ -10,6 +12,58 @@ struct TreeNode
     TreeNode(int _val) : val(_val), left(nullptr), right(nullptr) {}
     TreeNode(int _val, TreeNode *_left, TreeNode *_right) : val(_val), left(_left), right(_right) {}
 };
+
+/*
+501. 二叉搜索树中的众数
+已解答
+简单
+给你一个含重复值的二叉搜索树（BST）的根节点 root ，找出并返回 BST 中的所有 众数（即，出现频率最高的元素）。
+如果树中有不止一个众数，可以按 任意顺序 返回。
+*/
+void searchBST(TreeNode *cur, unordered_map<int, int> &map)
+{
+    if (cur == nullptr)
+        return;
+    map[cur->val]++;
+    searchBST(cur->left, map);
+    searchBST(cur->right, map);
+    return;
+}
+static bool cmp(pair<int, int> &a, pair<int, int> &b)
+{
+    return a.second > b.second;
+}
+vector<int> findMode(TreeNode *root)
+{
+    unordered_map<int, int> map;
+    vector<int> result;
+    if (root == nullptr)
+        return result;
+    searchBST(root, map);
+
+    vector<pair<int, int>> vec(map.begin(), map.end());
+    sort(vec.begin(), vec.end(), cmp);
+    result.push_back(vec[0].first);
+    for (int i = 1; i < vec.size(); i++)
+    {
+        // 取最高的放到数组中
+        if (vec[i].second == vec[0].second)
+            result.push_back(vec[i].first);
+        else
+            break;
+    }
+    return result;
+}
+
+// struct TreeNode
+// {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int _val) : val(_val), left(nullptr), right(nullptr) {}
+//     TreeNode(int _val, TreeNode *_left, TreeNode *_right) : val(_val), left(_left), right(_right) {}
+// };
 
 /*
 时间:20250815 15:47
